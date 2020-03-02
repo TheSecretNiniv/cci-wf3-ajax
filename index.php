82d34329433fba86201352fae81e025ee3d70c72
TheSecretNiniv <thesecretniniv@ntymail.com>
2020-03-02 11:28:21 +0100

    Creation fichier gitignore

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajax</title>
+    <script src="./assets/js/ajax.js"></script>
 </head>
 <body>
-  
+  <script>
+    console.log(ajax());
+  </script>
 
 
 </body>
